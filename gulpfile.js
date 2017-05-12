baa43183b5917570737495918ab1520ed4f6d687
flomaster <flomaster9@mail.ru>
2017-05-13 01:10:35 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
+var vueify = require('gulp-vueify');
+
+
+gulp.task('build-js', function() {
+    gulp.src('./scripts/app.js')
+        .pipe(browserify())
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('build-vue', function() {
+    gulp.src('./components/**/*.vue')
+        .pipe(vueify())
+        .pipe(gulp.dest('./scripts/components/'));
+});
+
+gulp.task('watcher',['build-vue', 'build-js'], function() {
+    gulp.watch('./scripts/**/*.js', ['build-js']);
+      gulp.watch('./components/**/*.vue', ['build-vue']);
+    gulp.watch('./index.html').on('change', browserSync.reload);
+    gulp.watch('./app.js').on('change', browserSync.reload);
+});
+
+gulp.task('serve',['watcher'], function() {
+    browserSync.init({
+        server: {
+            baseDir: './',
+            open: true
+        }
+    })
+});
---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+var Vue = require('../node_modules/vue/dist/vue.min.js');
+
+
+var app = new Vue({
+	el: '.app',
+
+	components: {
+		
+	},
+
+	data: {
+		
+	},
+
+	methods: {
+		
+		}
+
+	},
+})
